5e3243a0c38a305550d8b264ff324be20c36fe26
Blake Bradford <bbradforddesign@gmail.com>
2022-01-16 19:13:38 -0600

    configuring contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+    // configure credentials
+    const client = createClient({
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        space: process.env.CONTENTFUL_SPACE_ID,
+    });
+
+    // retrieve by content type
+    const res = await client.getEntries({ content_type: "recipe" });
+
+    // pass data to component to render
+    return {
+        props: {
+            recipes: res.items,
+        },
+    };
+}
+
+export default function Recipes({ recipes }) {
+    console.log(recipes);
+    return <div className="recipe-list">Recipe List</div>;
 }
